8695f2bfd088f7bc3595b4872cd5260b73f12ded
simoneholder1 <31088442+simoneholder1@users.noreply.github.com>
2017-10-31 10:04:46 -0700

    db created

---
+++ waypoint/Waypoint-Homes/db/init.sql
@@ -0,0 +1,38 @@
+CREATE TABLE LoginInfo(
+    Id Primary Key Int Not Null
+    email VARCHAR(100),
+    password VARCHAR(20)
+)
+
+CREATE TABLE UserFaves(
+    Id
+    email VARCAR(100),
+    Favorites 
+)
+
+CREATE TABLE Code(
+    email VARCHAR(100),
+    propID INTEGER,
+    code INTEGER,
+    showdate YYYY-MM-DD,
+    showtime HH:MM,
+    ampm 
+)
+
+CREATE TABLE UserInfo(
+    email
+    FirstName
+    LastName
+    Phone
+    ShowDate
+    ShowTime
+    PropID
+    
+)
+
+CREATE TABLE Calendar(
+    PropID
+    showdate
+    showtime
+)
+
